8f2f7fe85a12f32ad475544b33a666779baf76f8
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-18 12:36:19 -0400

    Update SchemaBuilderTest.php

--- tests/SchemaBuilderTest.php
+++ tests/SchemaBuilderTest.php
@@ -1,7 +1,21 @@
 <?php
 
 use Doctrine\DBAL\Schema\Schema;
-use FosUserUtil\Doctrine\DBAL\SchemaBuilder as  UserSchemaBuilder;
+// use FosUserUtil\Doctrine\DBAL\SchemaBuilder as  UserSchemaBuilder;
 use PHPUnit\Framework\TestCase;
 
-(new UserSchemaBuilder(new Schema()))->UserTable();
+/**
+ * @covers SchemaBuilder
+ */
+final class SchemaBuilderTest extends TestCase
+{
+    public function testCanBeCreatedFromValidSchema()
+    {
+        $this->assertInstanceOf(
+            SchemaBuilder::class,
+            new SchemaBuilder(new Schema())
+        );
+    }
+}
+
+// (new UserSchemaBuilder())->UserTable();
