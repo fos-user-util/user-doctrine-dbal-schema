53d83d6ca3a59761ee37b68b489972733bf12005
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-20 14:43:27 -0400

    Update SchemaBuilderTest.php

--- tests/SchemaBuilderTest.php
+++ tests/SchemaBuilderTest.php
@@ -15,7 +15,7 @@ use FosUserUtil\Doctrine\DBAL\SchemaBuilder;
 use PHPUnit\Framework\TestCase;
 
 /**
- * @covers \SchemaBuilder
+ * @covers SchemaBuilder
  */
 final class SchemaBuilderTest extends TestCase
 {
